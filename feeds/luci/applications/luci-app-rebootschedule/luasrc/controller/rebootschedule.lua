6f65188cf4a06fa4797706c37f1a851f0cf412da
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-19 20:38:56 +0800

    Update rebootschedule.lua

--- feeds/luci/applications/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
+++ feeds/luci/applications/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
@@ -4,7 +4,7 @@ function index()
 		return
 	end
 	
-	entry({"admin", "system"}, firstchild(), "system", 44).dependent = false
+	entry({"admin", "system"}, firstchild(), "系统", 44).dependent = false
 	entry({"admin", "system", "rebootschedule"}, cbi("rebootschedule"), "定时设置", 20).dependent = true
 end
 
